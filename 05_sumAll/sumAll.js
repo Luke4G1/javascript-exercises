13c7d25cd2fb40acbb1bf27775cdcb73ccaa7a0a
Luke4G1 <theooldlyon@gmail.com>
2022-07-19 12:37:12 +0200

    EXCERCISES DONE

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+    let sum = 0
+    switch (true) {
+        case (isNaN(n1)):
+            return "ERROR"
+            break;
+        case (isNaN(n2)):
+            return "ERROR"
+            break;
+        case (n2 < n1):
+            for (n2; n2 <= n1; n2--) {
+                sum += n2
+            }
+            return sum
+            break;
+        case (n1 < 0 || n2 < 0):
+            return "ERROR"
+            break;
+        default:
 
-};
-
+            for (n1; n1 <= n2; n1++) {
+                sum += n1
+            }
+            return sum;
+    }
+}
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (grades) {
+  let toc = ((grades - 32) / 1.8)
+  return parseFloat(toc.toFixed(1))
 };
 
-const ctof = function() {
+const ctof = function (grades) {
+  let tof = (grades * 1.8 + 32)
+  return parseFloat(tof.toFixed(1))
 
 };
 
